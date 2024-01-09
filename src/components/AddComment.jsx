0e60dec3cd3093fbdeebf8437391234efba711e5
MelissaCanu <melissamcanu@gmail.com>
2024-01-09 16:21:31 +0100

    refactor addComments done

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,105 +1,103 @@
-import { Component } from 'react'
-import { Button, Form } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
 
-class AddComment extends Component {
-  state = {
-    comment: {
-      comment: '',
-      rate: 1,
-      elementId: this.props.asin,
-    },
-  }
+const AddComment = (props) => {
+	/* il parametro sono le proprietà da passare al componente */
+	const [comment, setComment] = useState({
+		comment: "",
+		rate: 1,
+		elementId: props.asin,
+	});
+};
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.asin !== this.props.asin) {
-      this.setState({
-        comment: {
-          ...this.state.comment,
-          elementId: this.props.asin,
-        },
-      })
-    }
-  }
+useEffect(() => {
+	setComment((prevComment) => ({ ...prevComment, elementId: props.asin }));
+}, [props.asin]);
 
-  sendComment = async (e) => {
-    e.preventDefault()
-    try {
-      let response = await fetch(
-        'https://striveschool-api.herokuapp.com/api/comments',
-        {
-          method: 'POST',
-          body: JSON.stringify(this.state.comment),
-          headers: {
-            'Content-type': 'application/json',
-            Authorization: 'Bearer inserisci-qui-il-tuo-token',
-          },
-        }
-      )
-      if (response.ok) {
-        alert('Recensione inviata!')
-        this.setState({
-          comment: {
-            comment: '',
-            rate: 1,
-            elementId: this.props.asin,
-          },
-        })
-      } else {
-        throw new Error('Qualcosa è andato storto')
-      }
-    } catch (error) {
-      alert(error)
-    }
-  }
+// componentDidUpdate(prevProps) {
+//   if (prevProps.asin !== this.props.asin) {
+//     this.setState({
+//       comment: {
+//         ...this.state.comment,
+//         elementId: this.props.asin,
+//       },
+//     })
+//   }
+// }
 
-  render() {
-    return (
-      <div className="my-3">
-        <Form onSubmit={this.sendComment}>
-          <Form.Group className="mb-2">
-            <Form.Label>Recensione</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Inserisci qui il testo"
-              value={this.state.comment.comment}
-              onChange={(e) =>
-                this.setState({
-                  comment: {
-                    ...this.state.comment,
-                    comment: e.target.value,
-                  },
-                })
-              }
-            />
-          </Form.Group>
-          <Form.Group className="mb-2">
-            <Form.Label>Valutazione</Form.Label>
-            <Form.Control
-              as="select"
-              value={this.state.comment.rate}
-              onChange={(e) =>
-                this.setState({
-                  comment: {
-                    ...this.state.comment,
-                    rate: e.target.value,
-                  },
-                })
-              }
-            >
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </Form.Control>
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Invia
-          </Button>
-        </Form>
-      </div>
-    )
-  }
-}
+sendComment = async (e) => {
+	e.preventDefault();
+	try {
+		let response = await fetch(
+			"https://striveschool-api.herokuapp.com/api/comments",
+			{
+				method: "POST",
+				body: JSON.stringify(comment),
+				headers: {
+					"Content-type": "application/json",
+					Authorization:
+						"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc1ZjY4YzNkYWRhMDAwMThhNjlmOTgiLCJpYXQiOjE3MDQ4MTI0MDEsImV4cCI6MTcwNjAyMjAwMX0.ydG0TJSjh5cL5KKBHxs1E3fdDSzaODYWadEQiMiXM1g",
+				},
+			}
+		);
+		if (response.ok) {
+			alert("Recensione inviata!");
+			setComment({
+				comment: {
+					comment: "",
+					rate: 1,
+					elementId: props.asin,
+				},
+			});
+		} else {
+			throw new Error("Qualcosa è andato storto");
+		}
+	} catch (error) {
+		alert(error);
+	}
+};
 
-export default AddComment
+return (
+	<div className="my-3">
+		<Form onSubmit={sendComment}>
+			<Form.Group className="mb-2">
+				<Form.Label>Recensione</Form.Label>
+				<Form.Control
+					type="text"
+					placeholder="Inserisci qui il testo"
+					value={comment.comment}
+					onChange={(e) =>
+						setComment((prevComment) => ({
+							...prevComment,
+							comment: e.target.value,
+						}))
+					}
+				/>
+			</Form.Group>
+			<Form.Group className="mb-2">
+				<Form.Label>Valutazione</Form.Label>
+				<Form.Control
+					as="select"
+					value={comment.rate}
+					onChange={(e) =>
+						setComment((prevComment) => ({
+							...prevComment,
+							rate: e.target.value,
+						}))
+					}
+				>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+				</Form.Control>
+			</Form.Group>
+			<Button variant="primary" type="submit">
+				Invia
+			</Button>
+		</Form>
+	</div>
+);
+
+export default AddComment;
--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,39 +1,40 @@
-import { Button, ListGroup } from 'react-bootstrap'
+import { Button, ListGroup } from "react-bootstrap";
 
 const SingleComment = ({ comment }) => {
-  const deleteComment = async (asin) => {
-    try {
-      let response = await fetch(
-        'https://striveschool-api.herokuapp.com/api/comments/' + asin,
-        {
-          method: 'DELETE',
-          headers: {
-            Authorization: 'Bearer inserisci-qui-il-tuo-token',
-          },
-        }
-      )
-      if (response.ok) {
-        alert('La recensione è stata elimata!')
-      } else {
-        throw new Error('La recensione non è stata eliminata!')
-      }
-    } catch (error) {
-      alert(error)
-    }
-  }
+	const deleteComment = async (asin) => {
+		try {
+			let response = await fetch(
+				"https://striveschool-api.herokuapp.com/api/comments/" + asin,
+				{
+					method: "DELETE",
+					headers: {
+						Authorization:
+							"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc1ZjY4YzNkYWRhMDAwMThhNjlmOTgiLCJpYXQiOjE3MDQ4MTI0MDEsImV4cCI6MTcwNjAyMjAwMX0.ydG0TJSjh5cL5KKBHxs1E3fdDSzaODYWadEQiMiXM1g",
+					},
+				}
+			);
+			if (response.ok) {
+				alert("La recensione è stata elimata!");
+			} else {
+				throw new Error("La recensione non è stata eliminata!");
+			}
+		} catch (error) {
+			alert(error);
+		}
+	};
 
-  return (
-    <ListGroup.Item>
-      {comment.comment}
-      <Button
-        variant="danger"
-        className="ms-2"
-        onClick={() => deleteComment(comment._id)}
-      >
-        Elimina
-      </Button>
-    </ListGroup.Item>
-  )
-}
+	return (
+		<ListGroup.Item>
+			{comment.comment}
+			<Button
+				variant="danger"
+				className="ms-2"
+				onClick={() => deleteComment(comment._id)}
+			>
+				Elimina
+			</Button>
+		</ListGroup.Item>
+	);
+};
 
-export default SingleComment
+export default SingleComment;
